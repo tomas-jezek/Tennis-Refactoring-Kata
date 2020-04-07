22e2d32609dfdb0c3620ce604d19cc26fdf6f23f
Tomáš Ježek <tomas.jezek1@seznam.cz>
2020-04-08 01:52:32 +0200

    Inconsistent naming
    And VS/Resharper highlights member variables anyway, so there's no need for prefixes.

--- Tennis/TennisGame1.cs
+++ Tennis/TennisGame1.cs
@@ -2,8 +2,8 @@ namespace Tennis
 {
     class TennisGame1 : ITennisGame
     {
-        private int m_score1 = 0;
-        private int m_score2 = 0;
+        private int score1 = 0;
+        private int score2 = 0;
         private string player1Name;
         private string player2Name;
 
@@ -40,17 +40,17 @@ namespace Tennis
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
-                m_score1 += 1;
+                score1 += 1;
             else
-                m_score2 += 1;
+                score2 += 1;
         }
 
         public string GetScore()
         {
             string score = "";
-            if (m_score1 == m_score2)
+            if (score1 == score2)
             {
-                switch (m_score1)
+                switch (score1)
                 {
                     case 0:
                         score = "Love-All";
@@ -67,9 +67,9 @@ namespace Tennis
 
                 }
             }
-            else if (m_score1 >= 4 || m_score2 >= 4)
+            else if (score1 >= 4 || score2 >= 4)
             {
-                var minusResult = m_score1 - m_score2;
+                var minusResult = score1 - score2;
                 if (minusResult == 1) score = "Advantage player1";
                 else if (minusResult == -1) score = "Advantage player2";
                 else if (minusResult >= 2) score = "Win for player1";
@@ -79,8 +79,8 @@ namespace Tennis
             {
                 for (var i = 1; i < 3; i++)
                 {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
+                    if (i == 1) tempScore = score1;
+                    else { score += "-"; tempScore = score2; }
                     switch (tempScore)
                     {
                         case 0:
